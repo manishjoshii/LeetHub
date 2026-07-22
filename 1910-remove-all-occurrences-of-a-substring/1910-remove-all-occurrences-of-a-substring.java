f08fff6b64290c1a714dfc1ca1093af5b5704a17
Manish Joshi <manishjoshi073@gmail.com>
2026-07-22 14:52:35 +0530

    Time: 2 ms (84.16%), Space: 44.4 MB (51.99%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,54 +1,59 @@
 class Solution {
 
-    boolean check(Stack<Character> st, String part, int n) {
-        //create temp stack and copy original chars in it
-        Stack<Character> s = new Stack<>();
-        s.addAll(st);
+    //approach 1
 
-        for (int i = n - 1; i >= 0; i--) {
-            char ch = s.pop();
-            if (ch != part.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
-    }
+    // boolean check(Stack<Character> st, String part, int n) {
+    //     //create temp stack and copy original chars in it
+    //     Stack<Character> s = new Stack<>();
+    //     s.addAll(st);
 
-    public String removeOccurrences(String s, String part) {
-        Stack<Character> st = new Stack<>();
-        int m = s.length();
-        int n = part.length();
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         char ch = s.pop();
+    //         if (ch != part.charAt(i)) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 
-        // push chars without part in stack
-        for (int i = 0; i < s.length(); i++) {
-            st.push(s.charAt(i));
+    // public String removeOccurrences(String s, String part) {
+    //     Stack<Character> st = new Stack<>();
+    //     int m = s.length();
+    //     int n = part.length();
 
-            // check and remove part chars from stack
-            if (st.size() >= n && check(st, part, n) == true) {
-                for (int j = 0; j < n; j++) {
-                    st.pop();
-                }
-            }
-        }
+    //     // push chars without part in stack
+    //     for (int i = 0; i < s.length(); i++) {
+    //         st.push(s.charAt(i));
 
-        StringBuilder sb = new StringBuilder();
+    //         // check and remove part chars from stack
+    //         if (st.size() >= n && check(st, part, n) == true) {
+    //             for (int j = 0; j < n; j++) {
+    //                 st.pop();
+    //             }
+    //         }
+    //     }
 
-        while (!st.isEmpty()) {
-            sb.append(st.pop());
-        }
+    //     StringBuilder sb = new StringBuilder();
 
-        int l = 0, r = sb.length() - 1;
-        // reverse the string
-        while (l <= r) {
-            char c = sb.charAt(l);
-            sb.setCharAt(l, sb.charAt(r));
-            sb.setCharAt(r, c);
-            l++;
-            r--;
-        }
+    //     while (!st.isEmpty()) {
+    //         sb.append(st.pop());
+    //     }
 
-        return sb.toString();
-    }
+    //     int l = 0, r = sb.length() - 1;
+    //     // reverse the string
+    //     while (l <= r) {
+    //         char c = sb.charAt(l);
+    //         sb.setCharAt(l, sb.charAt(r));
+    //         sb.setCharAt(r, c);
+    //         l++;
+    //         r--;
+    //     }
+
+    //     return sb.toString();
+    // }
+
+    //approach 2
+
     // public String removeOccurrences(String s, String part) {
     //     StringBuilder sb = new StringBuilder(s);
     //     int i = 0;
@@ -62,4 +67,13 @@ class Solution {
     //     }
     //     return sb.toString();
     // }
+
+    //approach 3
+    public String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            int index = s.indexOf(part);
+            s = s.substring(0, index) + s.substring(index + part.length());
+        }
+        return s;
+    }
 }
