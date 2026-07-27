3c8e0c13bf3ce76ff1104b95060d26b167b7c947
Manish Joshi <manishjoshi073@gmail.com>
2026-07-27 15:12:10 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.16%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -12,43 +12,45 @@
 public class Solution {
 
     // brute force approach time = O(n) , space = O(n)
-    public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null)
-            return null;
-
-        Set<ListNode> set = new HashSet<>();
-
-        while (head != null) {
-            if (set.contains(head)) {
-                return head;
-            }
-            set.add(head);
-            head = head.next;
-        }
-        return head;
-    }
-
-    // optimal approach  Time: O(n) Space: O(1)
     // public ListNode detectCycle(ListNode head) {
     //     if (head == null || head.next == null)
     //         return null;
 
-    //     ListNode slow = head.next;
-    //     ListNode fast = head.next.next;
+    //     Set<ListNode> set = new HashSet<>();
 
-    //     while (slow.val != fast.val) {
-    //         slow = slow.next;
-    //         fast = fast.next.next;
-    //         if (fast == null || slow == null)
-    //             return null;
+    //     while (head != null) {
+    //         if (set.contains(head)) {
+    //             return head;
+    //         }
+    //         set.add(head);
+    //         head = head.next;
     //     }
-
-    //     slow = head;
-    //     while (slow.val != fast.val) {
-    //         slow = slow.next;
-    //         fast = fast.next;
-    //     }
-
-    //     return slow;
+    //     return head;
     // }
+
+    // optimal approach  Time: O(n) Space: O(1)
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+
+        }
+
+        return null;
+    }
 }
