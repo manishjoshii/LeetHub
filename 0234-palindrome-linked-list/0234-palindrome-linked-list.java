30a0847c903b708771fe1362bf568e8469a0beb5
Manish Joshi <manishjoshi073@gmail.com>
2026-07-29 11:43:33 +0530

    Time: 15 ms (9.14%), Space: 102 MB (5.55%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,34 +9,57 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
+    ListNode curr;
 
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = null;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-
-            ListNode temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
+    public boolean solve(ListNode head) {
+        if (head == null) {
+            return true;
         }
 
-        // odd length list  ex [1,2,1]
-        if (fast != null) {
-            slow = slow.next;
+        boolean ans = solve(head.next);
+
+        if (head.val != curr.val) {
+            return false;
         }
 
-        while (prev != null && slow != null) {
-            if (prev.val != slow.val) {
-                return false;
-            }
-            prev = prev.next;
-            slow = slow.next;
-        }
+        curr = curr.next;
 
-        return true;
+        return ans;
     }
+
+    public boolean isPalindrome(ListNode head) {
+        curr = head;
+        return solve(head);
+    }
+
+    // public boolean isPalindrome(ListNode head) {
+
+    //     ListNode slow = head;
+    //     ListNode fast = head;
+    //     ListNode prev = null;
+
+    //     while (fast != null && fast.next != null) {
+    //         fast = fast.next.next;
+
+    //         ListNode temp = slow.next;
+    //         slow.next = prev;
+    //         prev = slow;
+    //         slow = temp;
+    //     }
+
+    //     // odd length list  ex [1,2,1]
+    //     if (fast != null) {
+    //         slow = slow.next;
+    //     }
+
+    //     while (prev != null && slow != null) {
+    //         if (prev.val != slow.val) {
+    //             return false;
+    //         }
+    //         prev = prev.next;
+    //         slow = slow.next;
+    //     }
+
+    //     return true;
+    // }
 }
