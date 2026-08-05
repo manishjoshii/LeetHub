8add50bd434b0ed92546d0f5f710f4c2d679e805
Manish Joshi <manishjoshi073@gmail.com>
2026-08-05 11:19:40 +0530

    Time: 0 ms (100%), Space: 43.2 MB (30.08%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,29 +10,52 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || left == right || right < left)
+        if (head == null || head.next == null)
             return head;
 
-        Stack<Integer> st = new Stack<>();
-        ListNode curr = head;
-        int i = 1;
-        while (i <= right && curr != null) {
-            if (i >= left) {
-                st.push(curr.val);
-            }
-            curr = curr.next;
-            i++;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
         }
 
-        int j = 1;
-        ListNode node = head;
-        while (j <= right && node != null) {
-            if (j >= left && !st.isEmpty()) {
-                node.val = st.pop();
-            }
-            node = node.next;
-            j++;
+        ListNode curr = prev.next;
+
+        for (int i = 1; i <= right - left; i++) {
+            ListNode temp = prev.next;
+            prev.next = curr.next;
+            curr.next = curr.next.next;
+            prev.next.next = temp;
         }
-        return head;
+        return dummy.next;
     }
+    // public ListNode reverseBetween(ListNode head, int left, int right) {
+    //     if (head == null || left == right || right < left)
+    //         return head;
+
+    //     Stack<Integer> st = new Stack<>();
+    //     ListNode curr = head;
+    //     int i = 1;
+    //     while (i <= right && curr != null) {
+    //         if (i >= left) {
+    //             st.push(curr.val);
+    //         }
+    //         curr = curr.next;
+    //         i++;
+    //     }
+
+    //     int j = 1;
+    //     ListNode node = head;
+    //     while (j <= right && node != null) {
+    //         if (j >= left && !st.isEmpty()) {
+    //             node.val = st.pop();
+    //         }
+    //         node = node.next;
+    //         j++;
+    //     }
+    //     return head;
+    // }
 }
