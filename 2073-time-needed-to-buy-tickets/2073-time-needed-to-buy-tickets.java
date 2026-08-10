4222a6c041f885d8b7555778f408534887ea4a9b
Manish Joshi <manishjoshi073@gmail.com>
2026-08-10 11:12:32 +0530

    Time: 0 ms (100%), Space: 43 MB (75.38%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count = 0;
+
+        for (int i = 0; i < tickets.length; i++) {
+
+            if (i <= k) {
+                count += Math.min(tickets[i], tickets[k]);
+            } else {
+                count += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return count;
+    }
+}
