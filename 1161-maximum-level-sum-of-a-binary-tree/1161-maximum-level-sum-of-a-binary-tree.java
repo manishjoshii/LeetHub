29744d70a100f5528bbc20141f58ec8658f7e288
Manish Joshi <manishjoshi073@gmail.com>
2026-08-21 17:31:04 +0530

    Time: 8 ms (96.38%), Space: 49.1 MB (73.99%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -14,37 +14,69 @@
  * }
  */
 class Solution {
+
+    LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+
     public int maxLevelSum(TreeNode root) {
         if (root == null)
             return 0;
 
-        int maxSum = Integer.MIN_VALUE;
+        dfs(root, 1);
+
         int maxLevel = 1;
-        int currLevel = 1;
+        int maxSum = Integer.MIN_VALUE;
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-            int levelSize = q.size();
-            int currSum = 0;
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = q.poll();
-                currSum += node.val;
-                if (node.left != null) {
-                    q.offer(node.left);
-                }
-                if (node.right != null) {
-                    q.offer(node.right);
-                }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxSum) {
+                maxLevel = entry.getKey();
+                maxSum = entry.getValue();
             }
-            if(maxSum<currSum){
-                maxSum = currSum;
-                maxLevel = currLevel;
-            }
-            currLevel++;
         }
+
         return maxLevel;
     }
+
+    void dfs(TreeNode root, int level) {
+        if (root == null)
+            return;
+
+        map.put(level, map.getOrDefault(level, 0) + root.val);
+
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+
+    // public int maxLevelSum(TreeNode root) {
+    //     if (root == null)
+    //         return 0;
+
+    //     int maxSum = Integer.MIN_VALUE;
+    //     int maxLevel = 1;
+    //     int currLevel = 1;
+
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.offer(root);
+
+    //     while (!q.isEmpty()) {
+    //         int levelSize = q.size();
+    //         int currSum = 0;
+
+    //         for (int i = 0; i < levelSize; i++) {
+    //             TreeNode node = q.poll();
+    //             currSum += node.val;
+    //             if (node.left != null) {
+    //                 q.offer(node.left);
+    //             }
+    //             if (node.right != null) {
+    //                 q.offer(node.right);
+    //             }
+    //         }
+    //         if(maxSum<currSum){
+    //             maxSum = currSum;
+    //             maxLevel = currLevel;
+    //         }
+    //         currLevel++;
+    //     }
+    //     return maxLevel;
+    // }
 }
