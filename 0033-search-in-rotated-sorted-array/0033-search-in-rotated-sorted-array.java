6437f0f82b70289ab1606477f7177c69570cabff
Manish Joshi <manishjoshi073@gmail.com>
2026-09-04 22:35:48 +0530

    Time: 0 ms (100%), Space: 43.5 MB (96.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,26 +5,25 @@ class Solution {
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
+            
             if (nums[mid] == target) {
                 return mid;
             }
 
             if (nums[low] <= nums[mid]) {
-                if (target >= nums[low] && target <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             } else {
-                if (target >= nums[mid] && target <= nums[high]) {
+                if (nums[mid] <= target && target <= nums[high]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 }
